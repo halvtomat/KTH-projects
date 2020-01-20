55d4e7590a5d08d9157655829fbd91f4331a6bfb
Daniel <danne_1998@hotmail.se>
2020-01-21 00:22:14 +0100

    errors found in ready-queue, fixes to be done

--- OS/Seminar-3/green.h
+++ OS/Seminar-3/green.h
@@ -15,7 +15,8 @@ typedef struct green_cond_t{
 } green_cond_t;
 
 typedef struct green_mutex_t{
-    
+    volatile int taken;
+    green_t *susp;
 } green_mutex_t;
 
 int green_create(green_t *thread, void *(*fun)(void*), void *arg);
@@ -25,3 +26,7 @@ int green_join(green_t *thread, void** val);
 void green_cond_init(green_cond_t *cond);
 void green_cond_wait(green_cond_t *cond);
 void green_cond_signal(green_cond_t *cond);
+
+int green_mutex_init(green_mutex_t *mutex);
+int green_mutex_lock(green_mutex_t *mutex);
+int green_mutex_unlock(green_mutex_t *mutex);
--- OS/Seminar-3/test.c
+++ OS/Seminar-3/test.c
@@ -4,6 +4,10 @@
 
 #define LOOP 10
 
+int flag = 0;
+green_cond_t cond;
+green_mutex_t mutex;
+
 void *test(void *arg){
     int i = *(int*)arg;
     int loop = LOOP;
@@ -13,9 +17,6 @@ void *test(void *arg){
         green_yield();
     }
 }
-
-int flag = 0;
-green_cond_t cond;
 
 void *cond_test(void *arg){
     int id = *(int*)arg;
@@ -36,8 +37,46 @@ void *interrupt_test(void *arg){
     int id = *(int*)arg; 
     int loop = LOOP;
     while(loop > 0){
-        
+        if(flag == id){
+            printf("thread %d: %d\n", id, loop);
+            loop--;
+            flag = (id + 1) % 2;
+        }
     }
+}
+
+void *mutex_test_pass(void *arg){
+    int id = *(int*)arg;
+    int i;
+    
+    for(i = 0;i < 10000; i++){
+        green_mutex_lock(&mutex);
+        int temp = flag;
+        int j;
+        for(j = 0; j < 1000; j++){
+            j = j;
+        }
+        temp++;
+        flag = temp;
+        green_mutex_unlock(&mutex);
+    }
+    
+    printf("thread %d done, flag: %d\n", id, flag);
+}
+
+void *mutex_test_fail(void *arg){
+    int id = *(int*)arg;
+    int i;
+    for(i = 0;i < 10000; i++){
+        int temp = flag;
+        int j;
+        for(j = 0; j < 1000; j++){
+            j = j;
+        }
+        temp++;
+        flag = temp;
+    }
+    printf("thread %d done, flag: %d\n", id, flag);
 }
 
 int main(){
@@ -45,8 +84,10 @@ int main(){
     int a0 = 0;
     int a1 = 1;
     green_cond_init(&cond);
-    green_create(&g0, cond_test, &a0);
-    green_create(&g1, cond_test, &a1);
+    green_mutex_init(&mutex);
+    green_create(&g0, mutex_test_pass, &a0);
+    green_create(&g1, mutex_test_pass, &a1);
+    green_mutex_unlock(&mutex);
 
     green_join(&g0, NULL);
     green_join(&g1, NULL);
