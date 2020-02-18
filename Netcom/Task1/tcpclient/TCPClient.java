e2c2763f6648ce820b089076eab186baccf30b51
Daniel <danne_1998@hotmail.se>
2020-02-18 11:57:02 +0100

    started work on prog2 assignment 3

--- Netcom/Task1/tcpclient/TCPClient.java
+++ Netcom/Task1/tcpclient/TCPClient.java
@@ -7,6 +7,7 @@ public class TCPClient {
     
     public static String askServer(String hostname, int port, String ToServer) throws  IOException {
         Socket clientSocket = new Socket(hostname, port);
+        clientSocket.setSoTimeout(5000);
         byte[] clientBuffer = ToServer.getBytes(StandardCharsets.UTF_8);
 
         clientSocket.getOutputStream().write(clientBuffer, 0, clientBuffer.length);
@@ -21,6 +22,7 @@ public class TCPClient {
 
     public static String askServer(String hostname, int port) throws  IOException {
         Socket clientSocket = new Socket(hostname, port);
+        clientSocket.setSoTimeout(5000);
 
         String serverString = get_response(clientSocket);
         clientSocket.close();
@@ -28,7 +30,7 @@ public class TCPClient {
         return serverString;
     }
 
-    public static String get_response(Socket clientSocket) throws IOException {
+    private static String get_response(Socket clientSocket) throws IOException {
         byte[] serverBuffer = new byte[1024];
         int lastIndex = 0;
         while(true){
