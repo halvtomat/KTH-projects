a0bebfaefe839147c343e0cc5899dd11b7797667
Daniel <danne_1998@hotmail.se>
2020-02-20 12:24:53 +0100

    finished single method in trains.ex

--- Prog2/Assignment3/trains.ex
+++ Prog2/Assignment3/trains.ex
@@ -1,5 +1,33 @@
-defmodule moves do
+defmodule Moves do
 
-    def single({one,1},{[], [], []}), do:
+    import My_list
+    require My_list
+    
+
+    def single({:one, 1}, {m, l, r}) do {
+        take(m, length(m)-1), 
+        append(drop(m, length(m)-1), l),
+        r
+    }
+    end
+    def single({:one, -1}, {m, l, r}) do {
+        append(m, take(l, 1)), 
+        drop(l, 1),
+        r
+    }
+    end
+    def single({:two, 1}, {m, l, r}) do {
+        take(m, length(m)-1), 
+        l,
+        append(drop(m, length(m)-1), r)
+    }
+    end
+    def single({:two, -1}, {m, l, r}) do {
+        append(m, take(r, 1)), 
+        l,
+        drop(r, 1)
+    } 
+    end
+    
 
 end
