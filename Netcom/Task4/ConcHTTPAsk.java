1fd51dab6ca868b112cddd3590c764b2d526383c
Daniel <danne_1998@hotmail.se>
2020-03-06 12:10:48 +0100

    fixed naming of Task4 files

--- Netcom/Task4/ConcHTTPAsk.java
+++ Netcom/Task4/ConcHTTPAsk.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;
 import myrunnable.MyRunnable;
 
-public class ConchHTTPAsk {
+public class ConcHTTPAsk {
     public static void main( String[] args) throws IOException{
         int port = Integer.parseInt(args[0]);
         ServerSocket serverSocket = new ServerSocket(port);
